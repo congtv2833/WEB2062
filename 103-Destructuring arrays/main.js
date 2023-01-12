dc20d878987f3473becd0641ac6499f36218a61f
congtv <congt2603@gmail.com>
2023-01-12 12:24:35 +0700

    103

--- 103-Destructuring arrays/main.js
+++ 103-Destructuring arrays/main.js
@@ -1,11 +1,11 @@
-'use strict';
+`use strict`;
 
 const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic', 'Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    name: `Classico Italiano`,
+    location: `Via Angelo Tavanti 23, Firenze, Italy`,
+    categories: [`Italian`, `Pizzeria`, `Vegetarian`, `Organic`],
+    starterMenu: [`Focaccia`, `Bruschetta`, `Garlic`, `Bread`, `Caprese Salad`],
+    mainMenu: [`Pizza`, `Pasta`, `Risotto`],
 
     order : function(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
